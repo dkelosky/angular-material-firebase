744330eb8de94dd9ff7a9b4eaee0383223305687
Dan Kelosky <daniel.kelosky@ca.com>
2019-02-16 11:18:56 -0500

    in is messy

--- src/app/component/in/in.component.ts
+++ src/app/component/in/in.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, Input, ViewChild } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
 import { moveItemInArray, transferArrayItem, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ToggleSideNavService } from 'src/app/service/toggle-side-nav.service';
+import { MatSidenav } from '@angular/material';
 @Component({
   selector: 'app-in',
   templateUrl: './in.component.html',
   styleUrls: ['./in.component.less', '../../app.component.less']
 })
-export class InComponent implements OnInit {
+export class InComponent implements OnInit, OnDestroy {
+  private _mobileQueryListener: () => void;
+
+  @ViewChild('snav') snav: MatSidenav;
+
+  mobileQuery: MediaQueryList;
   todo = [
     'Daniel Kelosky',
     'Samuel Kelosky',
     'Abigail Kelosky',
+    'Abigail Kelosky',
+    // 'Abigail Kelosky',
+    // 'Abigail Kelosky',
+    // 'Abigail Kelosky',
+    // 'Abigail Kelosky',
+    // 'Abigail Kelosky',
+    // 'Abigail Kelosky',
   ];
 
   done = [
@@ -30,13 +44,28 @@ export class InComponent implements OnInit {
   }
   constructor(
     public afAuth: AngularFireAuth,
-  ) { }
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private toggle: ToggleSideNavService,
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+
+    this.toggle.toggle.subscribe(() => {
+      this.snav.toggle();
+    });
+  }
 
 
   ngOnInit() {
   }
 
   add() {
-    console.log('addd')
+    console.log('addd');
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 }
