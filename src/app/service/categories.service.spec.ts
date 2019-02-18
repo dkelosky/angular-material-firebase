87e9dd24d186aa9809ee5177aa2e0557c4b61ead
Dan Kelosky <daniel.kelosky@ca.com>
2019-02-17 20:39:19 -0500

    test compiler error

--- src/app/service/categories.service.spec.ts
+++ src/app/service/categories.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { CategoriesService } from './categories../categories.service
+import { CategoriesService } from './categories.service';
 
 describe('CategoriesService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
